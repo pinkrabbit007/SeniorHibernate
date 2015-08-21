bb8e8a19b700dc85536c7e1ee74d388a15b2b7e8
zhangjunqiao <350024806@qq.com>
2015-08-21 16:39:05 +0800

    居然可以自己实现了把JSON数据传出到页面上，今日最轻巧的一个复制粘贴，一步就成功了，不能想象……但是原理还没有搞懂诶……用的是jackson框架，logger的输入日志也会调了

--- src/main/java/com/test/test/controller/RespondbodyTestController.java
+++ src/main/java/com/test/test/controller/RespondbodyTestController.java
@@ -1,11 +1,17 @@
 package com.test.test.controller;
 
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fasterxml.jackson.core.JsonEncoding;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.test.test.domain.User;
 /**
  * 
@@ -15,17 +21,31 @@ import com.test.test.domain.User;
 @Controller
 @RequestMapping("/respondbody")
 public class RespondbodyTestController {
-
+	private static final Logger logger = LoggerFactory
+			.getLogger(RespondbodyTestController.class);
 	@RequestMapping(value = "/user/{userID}.do", method = { RequestMethod.POST,
 			RequestMethod.GET })
 	public @ResponseBody
 	User queryStudent(@PathVariable("userID") int userID) {
 
-		System.out.println("进入到@ResponseBody函数内部了，userID是"+userID);
+		System.out.println("进入到@ResponseBody函数内部了，userID是"+userID); 
 		User user = new User();
 		Long uid = new Long(userID);
 		user.setId(uid);
 		user.setUsername("乔治.W.布什");
+		
+		user.setIDcard("3307261989123481210");
+		user.setPhonenum("168699211710");
+		user.setGranted("1");
+		
+        //那么这里是如何把user这个实例转化为jason的呢？并没有传入user啊(⊙o⊙)…
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+             objectMapper.getJsonFactory().createJsonGenerator(System.out,JsonEncoding.UTF8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+		
 		return user;
 	}
 }
